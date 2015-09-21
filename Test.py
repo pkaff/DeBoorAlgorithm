b6f46367fcbd47d240d17c947525cb6e497155da
tpi11nfa <tpi11nfa@student.lu.se>
2015-09-21 16:40:07 +0200

    test fixed, hopefully for the last time :3

--- Test.py
+++ Test.py
@@ -8,7 +8,7 @@ class TestSpline(unittest.TestCase):
     def test_equalityX(self):
         #create the test spline
         grid = self.genX(20)
-        gp = list(grid)
+        gp = [0.0 , 0.0] + list(grid) + [19.9, 19.9]
         coeff = [(0.0, 0.0)]*(len(gp) - 2)
         k = 0
         for i in range(10):
@@ -24,15 +24,17 @@ class TestSpline(unittest.TestCase):
             coeff = spline.coeff[i]
             N_u = Spline.get_N(i, 3, spline.gp)(u)
             dN = (dN[0] + coeff[0]*N_u, dN[1] + coeff[1]*N_u)
+
+        #spline.plot()
         result = s_u[0]
         expected = dN[0]
-        self.assertAlmostEqual(result, expected, 0)
+        self.assertAlmostEqual(result, expected)
         
     #-same test, but for the "y" value
     def test_equalityY(self):
         #create the test spline
         grid = self.genX(20)
-        gp = list(grid)
+        gp = gp = [0.0 , 0.0] + list(grid) + [19.9, 19.9]
         coeff = [(0.0, 0.0)]*(len(gp) - 2)
         k = 0
         for i in range(10):
@@ -49,9 +51,10 @@ class TestSpline(unittest.TestCase):
             N_u = Spline.get_N(i, 3, spline.gp)(u)
             dN = (dN[0] + coeff[0]*N_u, dN[1] + coeff[1]*N_u)
         
+        #spline.plot()
         result = s_u[1]
         expected = dN[1]
-        self.assertAlmostEqual(result, expected, 0)
+        self.assertAlmostEqual(result, expected)
         
     def genX(self, n):
         k = 0.0
